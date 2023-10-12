6181fbba47e93eab4324e53cf29998f6ea859bd1
PriyankaG <110531641+PriyankaGiridharan123@users.noreply.github.com>
2023-10-12 14:40:06 +0530

    Delete Day 17 directory

--- Day 17/src/main/java/com/example/demo/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.demo.entity.Payment;
-
-public interface PaymentRepository extends CrudRepository<Payment, Integer>{
-
-}
